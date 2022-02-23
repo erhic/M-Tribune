c2778470c8b3c89a95fa342e1ee3397c92ef58a4
erhic <eric.gichovi@student.moringaschool.com>
2022-02-23 22:27:53 +0300

    registering app in the project in the settings of the entire project

--- tribune/settings.py
+++ tribune/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'news',
+    
+    'news.apps.NewsConfig',
 ]
 
 MIDDLEWARE = [
