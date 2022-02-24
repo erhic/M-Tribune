59cfecd1cfadb9ba4e3d2f65e12e5b6c8950da75
erhic <eric.gichovi@student.moringaschool.com>
2022-02-24 14:41:15 +0300

    updated models

--- news/models.py
+++ news/models.py
@@ -27,3 +27,7 @@ class Article(models.Model):
     editor = models.ForeignKey(Editor,on_delete=models.CASCADE)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+    
